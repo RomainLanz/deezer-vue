be7f350040aa5bba75d56d3fe387c5fbc76f59ae
Romain Lanz <romain.lanz+github@pm.me>
2021-12-12 20:06:37 +0100

    chore: setup for fresh start

---
+++ src/Application.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup></script>
+
+<template>
+	<p>Hello World</p>
+</template>
+
+<style lang="scss">
+	*,
+	*::before,
+	*::after {
+		box-sizing: border-box;
+	}
+
+	* {
+		margin: 0;
+	}
+
+	html,
+	body {
+		height: 100%;
+	}
+
+	body {
+		line-height: 1.5;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	img,
+	picture,
+	video,
+	canvas,
+	svg {
+		display: block;
+		max-width: 100%;
+	}
+
+	input,
+	button,
+	textarea,
+	select {
+		font: inherit;
+	}
+
+	p,
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		overflow-wrap: break-word;
+	}
+
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		isolation: isolate;
+	}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Application from './Application.vue';
 
-createApp(App).mount('#app')
+createApp(Application).mount('#app');
